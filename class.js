58f04630749113fbc6fecaea85f5c7b2761963eb
Sofiyah123 <musasophiat@gmail.com>
2024-01-24 04:11:53 -0800

    Added some javascript file

---
+++ class.js
@@ -0,0 +1,8 @@
+let isAMan = true;
+if (isAMan){
+    console.log("Yes, he is a man");
+}else{
+    console.log("No, he is not a man");
+}
+
+isAMan ? console.log("Yes, he is a man"):console.log("No, he is not a man")
